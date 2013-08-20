3b84d1ca85e31d0c0861a813e64fc3dc9a3d2fee
mbme <mbme@mbme>
2013-08-20 22:47:27 +0300

    added logging wrapping

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [ring-middleware-format "0.3.0"]]
+                 [ring-middleware-format "0.3.0"]
+                 [org.clojure/tools.logging "0.2.4"]
+                 [log4j/log4j "1.2.17"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler pimy.handler/app
          :auto-reload? true
-         :auto-refresh? true
-         :open-browser? false}
-  :profiles
-  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
+         :auto-refresh? true}
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
