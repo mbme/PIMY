7046d4a5c4b11162451410c42eead8e6a2d77bc6
mbme <ivan.symchych@gmail.com>
2013-08-20 23:03:19 +0300

    added exceptions processing

--- src/pimy/middleware.clj
+++ src/pimy/middleware.clj
@@ -1,7 +1,20 @@
 (ns pimy.middleware
   (:use compojure.core
+        ring.util.response
+        [cheshire.custom :only [JSONable]]
         [clojure.string :only [upper-case]])
-  (:require [clojure.tools.logging :as log]))
+  (:require [clojure.tools.logging :as log])
+  (:import (com.fasterxml.jackson.core JsonGenerator)))
+
+(defn wrap-exception-handler
+  [handler]
+  (fn [req]
+    (try
+      (handler req)
+      (catch Exception e
+        (->
+          (response e)
+          (status 500))))))
 
 (defn wrap-request-logger [handler]
   (fn [req]
@@ -9,10 +22,23 @@
       (log/debug remote-addr (upper-case (name request-method)) uri)
       (handler req))))
 
-(defn wrap-response-logger [handler]
+(defn wrap-response-logger
+  [handler]
   (fn [req]
     (let [response (handler req)
           {remote-addr :remote-addr request-method :request-method uri :uri} req
-          {status :status} response]
-      (log/debug remote-addr (upper-case (name request-method)) uri "->" status)
+          {status :status body :body} response]
+      (if (instance? Exception body)
+        (log/warn body remote-addr (upper-case (name request-method)) uri "->" status body)
+        (log/debug remote-addr (upper-case (name request-method)) uri "->" status))
       response)))
+
+(extend java.lang.Exception
+  JSONable
+  {:to-json (fn [^Exception e ^JsonGenerator jg]
+              (.writeStartObject jg)
+              (.writeFieldName jg "exception")
+              (.writeString jg (.getName (class e)))
+              (.writeFieldName jg "message")
+              (.writeString jg (.getMessage e))
+              (.writeEndObject jg))})
