ab8f16bb71ad1a5b1deeb2c833557d8cb1b72389
mbme <ivan.symchych@gmail.com>
2013-08-21 22:05:32 +0300

    simple properties using edn

--- src/pimy/handler.clj
+++ src/pimy/handler.clj
@@ -2,22 +2,39 @@
   (:use compojure.core
         ring.util.response
         pimy.middleware
-        [ring.middleware.format-params :only [wrap-restful-params]]
-        [ring.middleware.format-response :only [wrap-restful-response]])
+        [pimy.http :as http]
+        [ring.middleware.format-response :only [wrap-restful-response]]
+        [clojure.java.io :only [resource]])
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.edn :as edn]))
+
+(def config (edn/read-string (slurp (resource "properties.edn"))))
+(println (:version config))
 
 (defroutes api-routes
   (context "/api" []
     (OPTIONS "/" []
-      (->
-        (response {:version "0.1.0"})
-        (header "Allow" "OPTIONS")))
+      (http/options [:options ] {:version (:version config)}))
     (ANY "/" []
-      (->
-        (response nil)
-        (status 405)
-        (header "Allow" "OPTIONS"))))
+      (http/method-not-allowed [:options ]))
+    (context "/records" []
+      (GET "/" []
+        (http/not-implemented))
+      (GET "/:id" [id]
+        (http/not-implemented))
+      (HEAD "/:id" [id]
+        (http/not-implemented))
+      (POST "/" [:as req]
+        (http/not-implemented))
+      (PUT "/:id" [id]
+        (http/not-implemented))
+      (DELETE "/:id" [id]
+        (http/not-implemented))
+      (OPTIONS "/" []
+        (http/options [:options :get :head :put :post :delete ]))
+      (ANY "/" []
+        (http/method-not-allowed [:options :get :head :put :post :delete ]))))
   (route/not-found "Not found"))
 
 (def app
