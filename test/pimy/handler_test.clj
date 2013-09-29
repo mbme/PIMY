9e58f718685072e92143d8ab8682ede38ddd6944
mbme <mbme@mbme>
2013-09-29 12:26:21 +0300

    fixed test for /api get handler

--- test/pimy/handler_test.clj
+++ test/pimy/handler_test.clj
@@ -6,12 +6,12 @@
 (deftest test-api-routes
   (testing "API Options"
     (let [response (api-routes (request :options "/api"))]
-      (is (= (response :status) 200))
-      (is (contains? (response :body) :version))))
+      (is (= (response :status ) 200))
+      (is (contains? (response :body ) :version ))))
   (testing "API Get"
     (let [response (api-routes (request :get "/api"))]
-      (is (= (response :status) 405))
-      (is (nil? (response :body)))))
+      (is (= (response :status ) 200))
+      (is (contains? (response :body ) :version ))))
   (testing "Not Found"
     (let [response (api-routes (request :get "/invalid"))]
-      (is (= (response :status) 404)))))
+      (is (= (response :status ) 404)))))
