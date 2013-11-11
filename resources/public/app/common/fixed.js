97392963d37f2551f9ec68e227d64910ec6770ef
mbme <ivan.symchych@gmail.com>
2013-11-11 20:55:50 +0200

    general improvements

--- resources/public/app/common/fixed.js
+++ resources/public/app/common/fixed.js
@@ -5,7 +5,9 @@ define([
     'jquery',
     'lodash'
 ], function (app, $, _) {
-
+    var constants = {
+        BUTTON_TIMEOUT: 1000
+    };
     app.directive('fixed', function ($log) {
         return {
             restrict: 'A',
@@ -31,7 +33,8 @@ define([
                     css.left = pOffset.left + _.parseInt(attrs.left);
                 }
                 if (attrs.top) {
-                    //sometimes top offset is below zero, so we should use 0 instead
+                    //sometimes top offset is below zero,
+                    // so we should use 0 instead
                     css.top = (pOffset.top < 0 ? 0 : pOffset.top) + _.parseInt(attrs.top);
                 }
                 if (attrs.bottom) {
@@ -47,17 +50,19 @@ define([
                 var mousePos = { y: -1, x: -1 };
                 var buttonBarTimer;
 
+                /**
+                 *  @from EpicEditor
+                 * Here we check if the mouse has moves more than 5px
+                 * in any direction before triggering the mousemove code
+                 * we do this for 2 reasons:
+                 * 1. On Mac OS X lion when you scroll and it does
+                 * the iOS like "jump" when it hits the
+                 * top/bottom of the page it'll fire off a
+                 * mousemove of a few pixels depending on how hard you scroll
+                 * 2. We give a slight buffer to the user in case he
+                 * barely touches his touchpad or mouse and not trigger the UI
+                 */
                 var mouseMoveListener = function (e) {
-                    //@from EpicEditor
-                    // Here we check if the mouse has moves more than 5px
-                    // in any direction before triggering the mousemove code
-                    // we do this for 2 reasons:
-                    // 1. On Mac OS X lion when you scroll and it does
-                    //    the iOS like "jump" when it hits the
-                    //    top/bottom of the page it'll fire off a
-                    //    mousemove of a few pixels depending on how hard you scroll
-                    // 2. We give a slight buffer to the user in case he
-                    //    barely touches his touchpad or mouse and not trigger the UI
                     if (Math.abs(mousePos.y - e.pageY) >= 5 || Math.abs(mousePos.x - e.pageX) >= 5) {
                         fixedElem.show();
                         if (buttonBarTimer) {
@@ -66,7 +71,7 @@ define([
 
                         buttonBarTimer = window.setTimeout(function () {
                             fixedElem.hide();
-                        }, 1000);
+                        }, constants.BUTTON_TIMEOUT);
                     }
 
                     mousePos = { y: e.pageY, x: e.pageX };
